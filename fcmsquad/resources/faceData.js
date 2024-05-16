beaa0510a424c4abd42eea5e28484cddcf0ec929
Indvel <fbtkdqja112@gmail.com>
2024-05-16 20:26:44 +0900

    FCMSquad

--- fcmsquad/resources/faceData.js
+++ fcmsquad/resources/faceData.js
@@ -811,7 +811,8 @@ const faceData = [
         "idx": 70,
         "pid": "239602",
         "images": [
-            "https://indvel.github.io/fcmsquad/resources/players/players_23/p239602_JUV24.png",
+            "./resources/players/players_23/p239602_CT24_ICON.png",
+            "./resources/players/players_23/p239602_JUV24.png",
             "https://renderz.app/image-cdn/player_23_239602_TOTY24_ICON_cc30cb57f00ae9d9/normal",
             "https://renderz.app/image-cdn/player_23_239602/normal"
         ]
