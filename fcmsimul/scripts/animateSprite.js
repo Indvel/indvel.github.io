c0e1005b8787e079b146e0347cd78dfc0d1ba59a
Indvel <fbtkdqja112@gmail.com>
2024-05-18 16:17:09 +0900

    FCMSquad

--- fcmsimul/scripts/animateSprite.js
+++ fcmsimul/scripts/animateSprite.js
@@ -1,12 +1,12 @@
 var frameTimeCurrent = 0;
 
 class Sprites {
-    constructor(canvasWidth, canvasHeight, img, scale, spX, spY, maxFr, dX, dY) {
+    constructor(canvasWidth, canvasHeight, img, scale, spW, spH, spX, spY, maxFr, dX, dY) {
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
         this.image = img;
-        this.spriteWidth = 64;
-        this.spriteHeight = 64;
+        this.spriteWidth = spW;
+        this.spriteHeight = spH;
         this.width = this.spriteWidth;
         this.height = this.spriteHeight;
         this.scale = scale;
@@ -14,10 +14,11 @@ class Sprites {
         this.y = spY;
         this.minFrame = 0;
         this.maxFrame = maxFr;
+        this.frameCount = 0;
         this.frame = 0;
         this.frameX = 0;
         this.frameY = 0;
-        this.frameTime = 40;
+        this.frameTime = 3;
         this.mX = dX;
         this.mY = dY;
     }
@@ -27,15 +28,23 @@ class Sprites {
     }
     update() {
         frameTimeCurrent -= 16;
+        this.frameCount++;
         if(frameTimeCurrent <= 0) {
-            frameTimeCurrent += sprite.frameTime;
-            if(this.frame < 80) this.frame++;
-            else this.frame = this.minFrame;
-            this.frameX = this.frame % this.mX;
-            this.frameY = Math.floor(this.frame / this.mY);
+            frameTimeCurrent += this.frameTime;
+            
         }
+        if(this.frameCount > this.frameTime) {
+            this.frameCount = 0;
+            if(this.frame < this.maxFrame - 1) {
+                this.frame++;
+            } else {
+                this.frame = 0;
+            }
+        }
+        this.frameX = this.frame % this.mX;
+        this.frameY = Math.floor(this.frame / this.mY);
     }
-    clear() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    clear(context) {
+        context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     }
 }
