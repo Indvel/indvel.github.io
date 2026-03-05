976f0967b3fbc00f8145b5cdcfdc75aad1388189
Indvel <fbtkdqja112@gmail.com>
2026-03-05 18:23:10 +0900

    Added PRIMEM

--- prime/primeDashboard/script.js
+++ prime/primeDashboard/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     session = checkSessionAvailable();
     if(session == null) {
         alert("로그인 후 이용해주세요.");
-        history.back();
+        //history.back();
     } else {
         user = JSON.parse(sessionStorage.getItem(session));
         $('.userInfo').text(user.displayName);
